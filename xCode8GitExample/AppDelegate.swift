f2a03759159e58b1a5694d6c2388d88df6bb0128
Jonathan Levi <w1658912@h-lab-libf2-16.local>
2017-09-26 15:03:35 +0100

    Initial Commit

---
+++ xCode8GitExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  xCode8GitExample
+//
+//  Created by Jonathan Levi on 9/26/17.
+//  Copyright © 2017 Jonathan Levi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ xCode8GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  xCode8GitExample
+//
+//  Created by Jonathan Levi on 9/26/17.
+//  Copyright © 2017 Jonathan Levi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
